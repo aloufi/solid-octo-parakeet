6182a55067c4304b8790c21c87d6351273f285ab
aloufi <aloufi@me.com>
2019-10-06 09:23:06 +0300

    Update vacation.service.ts
    change the port

--- vacationInterface/src/app/vacation/vacationService/vacation.service.ts
+++ vacationInterface/src/app/vacation/vacationService/vacation.service.ts
@@ -11,7 +11,7 @@ import {VacationResponse} from '../model/vacationresponse';
 })
 export class VacationService {
 
-  apiURL = 'http://localhost:8099';
+  apiURL = 'http://localhost:8020';
 
   constructor(private httpClient: HttpClient) {}
 
